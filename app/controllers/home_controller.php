9b12dc073d3cdfb91cbee9b00118101950b9b9f9
Eshwar <eshwarcc@gmail.com>
2012-10-15 01:37:20 +0530

    1. Few canges in controller

--- app/controllers/home_controller.php
+++ app/controllers/home_controller.php
@@ -4,14 +4,10 @@ class HomeController extends ApplicationController {
 
 	public function index() {
 
-		$posts = Post::all();
+		$posts = Post::all( array( 'conditions' => "message LIKE '%hi%'", 'limit' => 10 ) );
 
 		View::render(array('view' => 'index',
-			'locals' => array(
-												'title' => 'Fly with PHP',
-												'content' => 'We flied with PHP',
-												'posts' => $posts
-											)
+			'locals' => array('title' => 'Fly PHP', 'content' => 'We flied with PHP',	'posts' => $posts)
 		));
 	}
 
