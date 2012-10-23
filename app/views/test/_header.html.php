6531311e816294284c9f9cef8ff0b48a2cebe60e
Eshwar <eshwarcc@gmail.com>
2012-10-23 16:42:56 +0530

    Added small logo

--- app/views/test/_header.html.php
+++ app/views/test/_header.html.php
@@ -1,5 +1,5 @@
 <div class="fly_header">
-	<?php image_tag('logo.png', array('width' => '50px', 'class' => 'fl ml10')); ?>
+	<?php image_tag('logo_small.png', array('width' => '50px', 'class' => 'fl ml10')); ?>
 	<h2 class="fl ml20"><?php echo $title; ?></h2>
 	<?php link_to ( 'Back', test_path, array('class' => 'fr lh50 mr30') ) ?>
 	<div class="clearfix"></div>
