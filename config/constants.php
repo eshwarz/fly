9f195ef551bca245fbd12d7a9d67503c03c97e42
Eshwar Chandra <eshwarcc@gmail.com>
2014-06-09 02:22:27 +0530

    Add more XSS filtering

--- config/constants.php
+++ config/constants.php
@@ -11,7 +11,7 @@ define('USER_SESSION_KEY_SALT', '+0p53cR3+');
 
 // less stylesheets are included on development environment, this can be set to true to enable less.watch() mode
 // can be set to false if less takes lots of time to compile
-defined('LESS_STYLE_ON_DEV') or define('LESS_STYLE_ON_DEV', true);
+defined('LESS_STYLE_ON_DEV') or define('LESS_STYLE_ON_DEV', false);
 
 // cookie constants
 define('COOKIE_PATH', '/');
--- config/initializers/input.php
+++ config/initializers/input.php
@@ -24,7 +24,12 @@ class Input
 	static function postParams ($key = null, $xss_filter = false) {
 		// getting only particular parameter if key is set otherwise return full array.
 		if (!empty($key)) {
-			return $_POST[$key];
+			$value = $_POST[$key];
+			// handling xss issues
+			if ($xss_filter) {
+				$value = self::xss_validate($value);
+			}
+			return $value;
 		} else {
 			return $_POST;
 		}
@@ -56,7 +61,35 @@ class Input
 	}
 
 	static function xss_validate ($str) {
-		$str = str_replace(array('<', '>'), array('&lt;', '&gt;'), $str);
+		// Fix &entity\n;
+		$str = str_replace(array('&amp;','&lt;','&gt;'), array('&amp;amp;','&amp;lt;','&amp;gt;'), $str);
+		$str = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $str);
+		$str = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $str);
+		$str = html_entity_decode($str, ENT_COMPAT, 'UTF-8');
+		 
+		// Remove any attribute starting with "on" or xmlns
+		$str = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $str);
+		 
+		// Remove javascript: and vbscript: protocols
+		$str = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $str);
+		$str = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $str);
+		$str = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $str);
+		 
+		// Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
+		$str = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $str);
+		$str = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $str);
+		$str = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $str);
+		 
+		// Remove namespaced elements (we do not need them)
+		$str = preg_replace('#</*\w+:\w[^>]*+>#i', '', $str);
+		 
+		do {
+			// Remove really unwanted tags
+			$old_data = $str;
+			$str = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $str);
+		} while ($old_data !== $str);
+		 
+		// returning the purified value
 		return $str;
 	}
 
